7ec4a468a945e0be27f24ad271f01c475d6e7054
kevindirkmunroe <kevin.munroe@gmail.com>
2026-04-26 11:20:03 -0700

    Log DB server url upon login

--- src/db.ts
+++ src/db.ts
@@ -3,9 +3,13 @@ import 'dotenv/config';
 
 // Retrieve the URL from an environment variable for security
 const connectionString = process.env.DATABASE_URL;
-
+if (!process.env.DATABASE_URL) {
+    throw new Error("❌ DATABASE_URL is not set");
+}
 const pool = new Pool({
     connectionString: connectionString
 });
+
+console.log("🟢 Connected to DB:", process.env.DATABASE_URL?.split("@")[1]);
 
 export default pool;
